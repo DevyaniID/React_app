7214b60cda8c2b3212c79538f938a444ec389ede
Devyani Ayare <ayaredevyani@gmail.com>
2025-09-15 20:09:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Header from "./component/Header";
+import Slider from "./component/Slider";
+import Footer from "./component/Footer";
+
+function App() {
+ 
+  return (
+    <>
+     <Header> </Header>
+     <Slider></Slider>
+     <Footer></Footer>
+    </>
+  )
+}
+
+export default App
